9af3f4039157cefaa4e83b0c4796cb5f2b56c37b
wltiii <myX2bear>
2022-10-22 21:55:28 -0500

    deleted wrongly placed library file added comments to correctly placed library file

--- lib/unrepresentable_state.dart
+++ lib/unrepresentable_state.dart
@@ -1,3 +1,23 @@
+/// This is the main library (barrel) file. It is placed directly under lib.
+/// Use it to provide developers with an overview of the entire public API of the package.
+/// Also, to avoid exposing more API than intended.
+///
+/// Add exports and use show to specify exactly which symbols you wish to expose. For example,
+/// given you wish to expose file 'add_chunked_encoding.dart' as 'addChunkedEncoding', and this
+/// file is located in folder 'middleware' which itself is located in the 'src' folder, you
+/// would write the following statement.
+///
+/// ```dart
+/// export 'src/middleware/add_chunked_encoding.dart' show addChunkedEncoding;
+/// ```
+///
+/// For more about organizing a library file, see:
+///   https://dart.dev/guides/libraries/create-library-packages#organizing-a-library-package
+///
+/// Document the package api well. See:
+///   https://dart.dev/guides/libraries/create-library-packages#documenting-a-library and
+///   https://dart.dev/guides/language/effective-dart/documentation
+///
 export 'src/auth_types/login_id.dart' show LoginId;
 export 'src/auth_types/name_space_url.dart' show NameSpaceUrl;
 export 'src/auth_types/password.dart' show Password;
